c6d0f7dfe409866ad4fdbb160d715cd2efb858e8
Daniel Bauer <daniel.bauer@intive.com>
2019-09-25 14:26:07 +0200

    fixed login

--- GarminExport/Auth/AuthService.cs
+++ GarminExport/Auth/AuthService.cs
@@ -1,6 +1,5 @@
 ﻿using RestSharp;
 using System;
-using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -9,6 +8,7 @@ namespace GarminExport.Auth
 {
     public class AuthService
     {
+        private static readonly string REFERER = "https://sso.garmin.com/sso/signin?service=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&webhost=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&source=https%3A%2F%2Fconnect.garmin.com%2Fsignin&redirectAfterAccountLoginUrl=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&redirectAfterAccountCreationUrl=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&gauthHost=https%3A%2F%2Fsso.garmin.com%2Fsso&locale=de_DE&id=gauth-widget&cssUrl=https%3A%2F%2Fstatic.garmincdn.com%2Fcom.garmin.connect%2Fui%2Fcss%2Fgauth-custom-v1.2-min.css&privacyStatementUrl=https%3A%2F%2Fwww.garmin.com%2Fde-DE%2Fprivacy%2Fconnect%2F&clientId=GarminConnect&rememberMeShown=true&rememberMeChecked=false&createAccountShown=true&openCreateAccount=false&displayNameShown=false&consumeServiceTicket=false&initialFocus=true&embedWidget=false&generateExtraServiceTicket=true&generateTwoExtraServiceTickets=false&generateNoServiceTicket=false&globalOptInShown=true&globalOptInChecked=false&mobile=false&connectLegalTerms=true&showTermsOfUse=false&showPrivacyPolicy=false&showConnectLegalAge=false&locationPromptShown=true&showPassword=true";
         public Session Session { get; private set; }
         private RestClient SSOClient { get; set; }
         private RestClient ConnectClient { get; set; }
@@ -49,15 +49,16 @@ namespace GarminExport.Auth
 
         private bool ProcessTicket(string ticketUrl)
         {
-            //http:\/\/connect.garmin.com\/post-auth\/login?ticket=ST-xyz
             var ticketUrlFormatted = ticketUrl.Replace(@"\/", @"/");
             var ticketId = ticketUrlFormatted.Substring(ticketUrlFormatted.LastIndexOf("ticket=") + 7);
             var uri = new Uri("https://connect.garmin.com/modern/?ticket=" + ticketId);
             var client = new RestClient(uri.Scheme + "://" + uri.Host)
             {
-                CookieContainer = Session.Cookies
+                CookieContainer = Session.Cookies,
+                FollowRedirects = true
             };
             var loginRequest = new RestRequest(uri.PathAndQuery, Method.GET);
+            loginRequest.AddHeader("Referer", REFERER);
             var response = client.Execute(loginRequest);
             return IsDashboardUri(response.ResponseUri);
         }
@@ -86,15 +87,15 @@ namespace GarminExport.Auth
             }
             var formDataBytes = Encoding.UTF8.GetBytes(formDataStr);
             request.AddParameter("application/x-www-form-urlencoded", formDataStr, ParameterType.RequestBody);
-            request.AddHeader("Referer", "https://sso.garmin.com/sso/signin");
+            request.AddHeader("Referer", REFERER);
             IRestResponse response = SSOClient.Execute(request);
             return response.Content;
         }
 
         private RestRequest BuildAuthRequest(Method method)
         {
-            var authRequest = new RestRequest("sso/signin", method);
-            authRequest.AddQueryParameter("service", "https://connect.garmin.com/modern/");
+            var authRequest = new RestRequest(REFERER, method);
+            authRequest.AddHeader("Referer", REFERER);
             return authRequest;
         }
 
